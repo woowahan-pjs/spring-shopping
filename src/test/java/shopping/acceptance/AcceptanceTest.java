e75d53b004f77dc632f521945c171054cba09cc1
woozi <kwj1270@naver.com>
2024-07-16 01:52:37 +0900

    feat: 테스트 컨테이너 환경 설정

---
+++ src/test/java/shopping/acceptance/AcceptanceTest.java
@@ -0,0 +1,28 @@
+package shopping.acceptance;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extensions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import shopping.utils.MySQLTestContainerExtension;
+
+
+@Extensions({@ExtendWith(MySQLTestContainerExtension.class)})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+        databaseCleanup.execute();
+    }
+}
