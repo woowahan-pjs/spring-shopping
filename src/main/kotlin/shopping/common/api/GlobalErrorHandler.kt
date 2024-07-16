08b11c6a75716dec435b8cff7fa690550302298a
who-is-hu <dydgndlaek97@gmail.com>
2024-07-17 01:30:02 +0900

    feat(common): 요청에 파라미터 안쓰는 경우 예외처리 추가

--- src/main/kotlin/shopping/common/api/GlobalErrorHandler.kt
+++ src/main/kotlin/shopping/common/api/GlobalErrorHandler.kt
@@ -1,9 +1,12 @@
 package shopping.common.api
 
+import com.fasterxml.jackson.databind.JsonMappingException
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -16,8 +19,30 @@ import shopping.common.error.ErrorMessage
 class GlobalErrorHandler {
     private val logger: Logger = LoggerFactory.getLogger(GlobalErrorHandler::class.java)
 
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun httpMessageNotReadableExceptionHandler(e: HttpMessageNotReadableException): ResponseEntity<ApiResponse<Unit>> {
+        logger.error("HttpMessageNotReadableException: {}", e.message, e)
+        val data =
+            when (val causeException = e.cause) {
+                is MismatchedInputException -> causeException.path[0].toData()
+                else -> null
+            }
+
+        val errorMessage =
+            ErrorMessage(
+                code = ErrorCode.VALIDATION_ERROR,
+                message = e.message ?: "Missing field error",
+                data = data,
+            )
+
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ApiResponse.error(errorMessage))
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun requestValidationExceptionHandler(e: MethodArgumentNotValidException): ResponseEntity<ApiResponse<Unit>> {
+        logger.error("MethodArgumentNotValidException: {}", e.message, e)
         val errorMessage =
             ErrorMessage(
                 code = ErrorCode.VALIDATION_ERROR,
@@ -65,4 +90,11 @@ private fun FieldError.toData() =
         "field" to field,
         "rejectedValue" to rejectedValue,
         "message" to defaultMessage,
+    )
+
+private fun JsonMappingException.Reference.toData() =
+    mapOf(
+        "field" to fieldName,
+        "rejectedValue" to null,
+        "message" to "null 이면 안됩니다.",
     )
