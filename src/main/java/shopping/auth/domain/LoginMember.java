2f16059be692b3e0b366e99f7ac34d399413febc
jylim <jylim@12cm.co.kr>
2024-07-16 17:06:06 +0900

    chore: auth 패키지 구성중

---
+++ src/main/java/shopping/auth/domain/LoginMember.java
@@ -0,0 +1,43 @@
+package shopping.auth.domain;
+
+import java.util.Objects;
+
+public class LoginMember {
+    private Long id;
+    private String email;
+    private Integer age;
+
+    public LoginMember() {
+    }
+
+    public LoginMember(Long id, String email, Integer age) {
+        this.id = id;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LoginMember that = (LoginMember) o;
+        return Objects.equals(id, that.id) && Objects.equals(email, that.email) && Objects.equals(age, that.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, age);
+    }
+}
