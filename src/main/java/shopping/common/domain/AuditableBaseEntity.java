b7f4d48a807d4b37d1f5a1b4e903e47fbd308640
hoa0217 <gjwjdghk123@naver.com>
2024-07-18 15:15:25 +0900

    refactor(BaseEntity): BaseEntity, AuditableBaseEntity 분리하여 추적 데이터 추가
    - BaseEntity: 생성시간, 수정시간
    - AuditableBaseEntity: 생성시간, 생성자, 수정시간, 수정자

--- src/main/java/shopping/common/domain/AuditableBaseEntity.java
+++ src/main/java/shopping/common/domain/AuditableBaseEntity.java
@@ -3,20 +3,18 @@ package shopping.common.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
-public abstract class BaseEntity {
+@EntityListeners(AuditingEntityListener.class)
+public class AuditableBaseEntity extends BaseEntity {
 
-    @CreatedDate
+    @CreatedBy
     @Column(updatable = false)
-    private LocalDateTime createdAt;
+    private String createdBy;
 
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
+    @LastModifiedBy
+    private String updatedBy;
 }
