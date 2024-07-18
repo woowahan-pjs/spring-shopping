9f0235fabddb2f1e40b1825f64d918b1741d4cdc
hoa0217 <gjwjdghk123@naver.com>
2024-07-18 15:51:49 +0900

    refactor(WishProduct): 위시 상품 엔티티 생성

---
+++ src/main/java/shopping/member/client/domain/WishProduct.java
@@ -0,0 +1,31 @@
+package shopping.member.client.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import shopping.common.domain.BaseEntity;
+
+@Entity
+@Table(name = "wish_products")
+public class WishProduct extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "wish_product_id", nullable = false)
+    private Long id;
+
+    private Long productId;
+
+    protected WishProduct() {
+    }
+
+    public WishProduct(final Long productId) {
+        this.productId = productId;
+    }
+
+    public boolean isSameProduct(WishProduct wishProduct){
+        return this.productId.equals(wishProduct.productId);
+    }
+}
