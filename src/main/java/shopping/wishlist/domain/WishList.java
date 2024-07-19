5cef03ea10eb9fcc408352919f8e855a2bac9f64
jylim <jylim@12cm.co.kr>
2024-07-19 22:32:17 +0900

    refactor: Wish 일급컬렉션 구현

--- src/main/java/shopping/wishlist/domain/WishList.java
+++ src/main/java/shopping/wishlist/domain/WishList.java
@@ -1,36 +1,24 @@
 package shopping.wishlist.domain;
 
 
-import jakarta.persistence.*;
-import lombok.*;
-import lombok.experimental.SuperBuilder;
-import shopping.BaseEntity;
-import shopping.product.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-@EqualsAndHashCode(callSuper = true)
 @Data
-@SuperBuilder
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-public class WishList extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long wishSn;
-
-    @Column(nullable = false)
-    private Long mbrSn;
-
-    @Column(nullable = false, columnDefinition = "bigint default 1")
+public class WishList {
     @Builder.Default
-    private Long cnt = 1L;
+    private List<Wish> wishList = new ArrayList<>();
 
-    @OneToOne
-    @JoinColumn(name = "prdctSn")
-    private Product product;
-
-    public Long getProductSn() {
-        return product.getPrdctSn();
+    public int getTotCnt() {
+        return wishList.size();
     }
 }
