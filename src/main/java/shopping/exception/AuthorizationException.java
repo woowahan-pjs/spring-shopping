280e538fdbe24fd93126359c3100f0197bf01c26
jylim <jylim@12cm.co.kr>
2024-07-19 13:54:46 +0900

    feat: exception 구현

---
+++ src/main/java/shopping/exception/AuthorizationException.java
@@ -0,0 +1,15 @@
+package shopping.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class AuthorizationException extends RuntimeException {
+    public AuthorizationException() {
+    }
+
+    public AuthorizationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/shopping/exception/ControllerExceptionHandler.java
@@ -0,0 +1,36 @@
+package shopping.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Void> handleIllegalArgsException() {
+        return badRequest();
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestException> handleBadRequestException(Exception e) {
+        return new ResponseEntity<>(new BadRequestException(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<NotFoundException> handleNotFoundException(Exception e) {
+        return new ResponseEntity<>(new NotFoundException(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(AuthorizationException.class)
+    public ResponseEntity<AuthorizationException> handleAuthorizationException(Exception e) {
+        return new ResponseEntity<>(new AuthorizationException(e.getMessage()), HttpStatus.UNAUTHORIZED);
+    }
+
+    private ResponseEntity<Void> badRequest() {
+        return ResponseEntity.badRequest().build();
+    }
+}
