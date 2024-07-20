4a6f8394d02bb675bf84edaefd03d37bda53f929
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-20 22:48:53 +0900

    feat(WishlistItems): 위시리스트 아이템 일급컬렉션 클래스 추가

---
+++ src/main/java/shopping/wishilist/domain/WishlistItems.java
@@ -0,0 +1,43 @@
+package shopping.wishilist.domain;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import shopping.product.domain.Product;
+import shopping.wishilist.exception.InvalidWishlistException;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Embeddable
+public class WishlistItems implements Iterable<Product> {
+    @OneToMany
+    @JoinColumn(name = "product_id")
+    private List<Product> products = new LinkedList<>();
+
+    protected WishlistItems() {
+    }
+
+    public void add(final Product product) {
+        if (products.contains(product)) {
+            throw new InvalidWishlistException("이미 존재하는 상품은 추가할 수 없습니다.");
+        }
+
+        this.products.add(product);
+    }
+
+    public void remove(final Product product) {
+        this.products.remove(product);
+    }
+
+    public Stream<Product> stream() {
+        return products.stream();
+    }
+
+    @Override
+    public Iterator<Product> iterator() {
+        return products.iterator();
+    }
+}
