396a2cdf2685975d7fab2842d9449199528044b8
SeolYoungKim <nasur4da@gmail.com>
2024-07-20 22:42:08 +0900

    build(Token): JWT 구현을 위한 의존성 추가 및 설정 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,10 @@ dependencies {
 
     runtimeOnly("com.h2database:h2")
 
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
