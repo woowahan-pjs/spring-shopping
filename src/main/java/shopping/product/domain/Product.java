efbe5a1ca34e867a03ac58f4d341b494457fda8b
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-20 12:00:23 +0900

    fix(Product): ProductName 사용하도록 변경

--- src/main/java/shopping/product/domain/Product.java
+++ src/main/java/shopping/product/domain/Product.java
@@ -8,18 +8,14 @@ import jakarta.persistence.Id;
 import org.springframework.util.StringUtils;
 import shopping.product.exception.ProductCreateException;
 
-import java.util.regex.Pattern;
-
 @Entity
 public class Product {
-    private static final Pattern ALLOWED_SPECIAL_CHARACTERS_PATTERN = Pattern.compile("^[a-zA-Z0-9 ()\\[\\]+\\-&/_]*$");
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String name;
+    @Column(nullable = false, length = ProductName.MIN_LENGTH)
+    private ProductName name;
 
     @Column(nullable = false)
     private String imagePath;
@@ -32,33 +28,14 @@ public class Product {
     }
 
     public Product(final String name, final String imagePath, final int amount, final long price) {
-        validate(name, imagePath, amount, price);
-        this.name = name;
+        validate(imagePath, amount, price);
+        this.name = new ProductName(name);
         this.imagePath = imagePath;
         this.amount = amount;
         this.price = price;
     }
 
-    private void validate(final String name, final String imagePath, final int amount, final long price) {
-        if (!StringUtils.hasText(name)) {
-            throw new ProductCreateException("상품 이름은 필수값 입니다.");
-        }
-
-        // 이름 길이 검증
-        if (name.length() > 15) {
-            throw new ProductCreateException("상품 이름은 15자 이하여야 합니다.");
-        }
-
-        // 특수 문자 검증
-        if (!ALLOWED_SPECIAL_CHARACTERS_PATTERN.matcher(name).matches()) {
-            throw new ProductCreateException("상품 이름에 허용되지 않는 특수 문자가 포함되어 있습니다.");
-        }
-
-//        // 비속어 필터링 (PurgoMalum API 사용)
-//        if (containsProfanity(name)) {
-//            throw new ProductCreateException("상품 이름에 비속어가 포함될 수 없습니다.");
-//        }
-
+    private void validate(final String imagePath, final int amount, final long price) {
         if (!StringUtils.hasText(imagePath)) {
             throw new ProductCreateException("상품 이미지는 필수값 입니다.");
         }
