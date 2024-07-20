a9cc3b6def1b0035347f7f7a9d6495aaa8a9df82
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-20 14:17:09 +0900

    feat(ProfanityChecker): 욕설 판별 클래스 추가

---
+++ src/main/java/shopping/product/exception/PurgoMalumRetrieveFailException.java
@@ -0,0 +1,7 @@
+package shopping.product.exception;
+
+public class PurgoMalumRetrieveFailException extends RuntimeException {
+    public PurgoMalumRetrieveFailException(final String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/shopping/product/infra/ProfanityCheckerTest.java
@@ -0,0 +1,33 @@
+package shopping.product.infra;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProfanityCheckerTest {
+
+    private ProfanityChecker profanityChecker;
+
+    @BeforeEach
+    void setUp() {
+        profanityChecker = new ProfanityChecker();
+    }
+
+    @Test
+    @DisplayName("특정 단어가 비속어가 아닌 경우 false 를 반환한다")
+    void containsProfanityTrueTest() {
+        final String word = "hello";
+
+        assertThat(profanityChecker.containsProfanity(word)).isFalse();
+    }
+
+    @Test
+    @DisplayName("특정 단어가 비속어인 경우 true 를 반환한다")
+    void containsProfanityFalseTest() {
+        final String word = "병신";
+
+        assertThat(profanityChecker.containsProfanity(word)).isTrue();
+    }
+}
