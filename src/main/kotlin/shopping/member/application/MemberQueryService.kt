9ae90322e42fdab206883a68ddcc5f5578b2af45
junseo <djawnstj44@naver.com>
2024-07-27 19:08:41 +0900

    feat: MEmberQueryService Transactional 애너테이션 추가

--- src/main/kotlin/shopping/member/application/MemberQueryService.kt
+++ src/main/kotlin/shopping/member/application/MemberQueryService.kt
@@ -1,11 +1,13 @@
 package shopping.member.application
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import shopping.global.exception.ApplicationException
 import shopping.global.exception.ErrorCode
 import shopping.member.domain.Member
 
 @Service
+@Transactional(readOnly = true)
 class MemberQueryService(
     private val memberQueryRepository : MemberQueryRepository
 ) {
