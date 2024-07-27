e08c64b2b6d2d7fa8f39d8deeb19c1118182975c
who-is-hu <dydgndlaek97@gmail.com>
2024-07-28 01:08:41 +0900

    test(member): 멤버 회원가입 로그인 테스트

--- src/test/kotlin/shopping/E2ETest.kt
+++ src/test/kotlin/shopping/E2ETest.kt
@@ -1,8 +1,8 @@
 package shopping
 
 import io.restassured.RestAssured
-import jakarta.transaction.Transactional
 import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.test.context.ActiveProfiles
@@ -11,14 +11,17 @@ import org.springframework.test.context.TestConstructor
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-@Transactional
 abstract class E2ETest {
     @LocalServerPort
     protected var port: Int = 0
+
+    @Autowired
+    private lateinit var databaseCleaner: DatabaseCleaner
 
     @BeforeEach
     fun setup() {
         RestAssured.port = port
         RestAssured.baseURI = "http://localhost"
+        databaseCleaner.clean()
     }
 }
