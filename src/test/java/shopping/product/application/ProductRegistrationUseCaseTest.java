50d0d8405650574a09fed894c745816100792162
woozi <kwj1270@naver.com>
2024-07-28 18:05:07 +0900

    test: 상품 이름이 영문 욕설이 들었다면 상품을 등록할 수 없다

--- src/test/java/shopping/product/application/ProductRegistrationUseCaseTest.java
+++ src/test/java/shopping/product/application/ProductRegistrationUseCaseTest.java
@@ -3,6 +3,7 @@ package shopping.product.application;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import shopping.common.exception.ProductNameInvalidException;
 import shopping.product.application.command.ProductRegistrationCommand;
 import shopping.product.application.query.ProductRegistrationQuery;
 import shopping.product.domain.ProductValidator;
@@ -10,6 +11,7 @@ import shopping.product.domain.repository.ProductRepository;
 import shopping.utils.fake.FakeProductRepository;
 import shopping.utils.fixture.ProductFixture;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -51,5 +53,24 @@ public class ProductRegistrationUseCaseTest {
                 () -> assertThat(productRegistrationQuery.sellerId()).isEqualTo(ProductFixture.SELLER_ID),
                 () -> assertThat(productRegistrationQuery.detailedImageUrls()).isEqualTo(ProductFixture.DETAILED_IMAGE_URLS)
         );
+    }
+
+    @DisplayName("상품 이름이 영문 욕설이 들었다면 상품을 등록할 수 없다")
+    @Test
+    void registerProductInvalidProductNameContainsProfanity() {
+        productValidator = new ProductValidator(word -> true);
+        productRegistrationUseCase = new ProductService(productRepository, productValidator);
+
+        final ProductRegistrationCommand command = new ProductRegistrationCommand(
+                ProductFixture.PROFANITY_NAME,
+                ProductFixture.AMOUNT,
+                ProductFixture.THUMBNAIL_IMAGE_URL,
+                ProductFixture.SUB_CATEGORY_ID,
+                ProductFixture.SHOP_ID,
+                ProductFixture.SELLER_ID,
+                ProductFixture.DETAILED_IMAGE_URLS
+        );
+        assertThatThrownBy(() -> productRegistrationUseCase.register(command))
+                .isExactlyInstanceOf(ProductNameInvalidException.class);
     }
 }
