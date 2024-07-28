7affbdfc782495c075a2857622ff642c2c37bda7
woozi <kwj1270@naver.com>
2024-07-28 13:01:56 +0900

    feat: 클라이언트 통신을 위한 CORS 적용

--- src/main/java/shopping/common/config/MvcConfig.java
+++ src/main/java/shopping/common/config/MvcConfig.java
@@ -2,6 +2,7 @@ package shopping.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import shopping.common.auth.AuthorizationArgumentResolver;
 
@@ -19,5 +20,11 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authorizationArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+        WebMvcConfigurer.super.addCorsMappings(registry);
     }
 }
