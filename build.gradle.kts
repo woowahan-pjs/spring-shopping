e14ad6c504780a5302afb3d355c43c80cd421982
zinzo <zinzoddari@gmail.com>
2026-03-02 22:12:59 +0900

    feat: Lombok 의존성 추가
    - build.gradle.kts에 lombok 의존성 추가
    
    (cherry picked from commit b62288beef5bdeb5a68bdc1ede3a01c8d8996926)

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,10 @@ dependencies {
 
     // swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.16")
+
+    // lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
