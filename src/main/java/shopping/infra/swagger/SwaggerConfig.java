0d67cb5c3fcfa80beec20a2dd693039c99768446
zinzo <zinzoddari@gmail.com>
2026-03-03 22:05:37 +0900

    feat: SwaggerConfig에 JWT SecurityScheme 추가
    - SwaggerConfig에 JWT를 위한 SecurityScheme 어노테이션 추가
    - 패키지 경로를 infra에서 infra.swagger로 변경하여 구조 정리
    - OpenAPIDefinition에 보안 스키마 포함을 위한 준비 완료

--- src/main/java/shopping/infra/swagger/SwaggerConfig.java
+++ src/main/java/shopping/infra/swagger/SwaggerConfig.java
@@ -1,11 +1,17 @@
-package shopping.infra;
+package shopping.infra.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-// TODO: security 설정 추가 필요
 @OpenAPIDefinition(
         info = @Info(title = "Shopping API", description = "[과제4] Shopping API 문서"),
         servers = {@Server(url = "http://localhost:8080", description = "local")})
+@SecurityScheme(
+        name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT")
 class SwaggerConfig {}
