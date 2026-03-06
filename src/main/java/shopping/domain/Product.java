da93bbc1ed009aa533cbb97e3db6b9224d2c4d99
apdkeb <apdkeb@naver.com>
2026-03-06 14:41:20 +0900

    feat(Product) : 상품 도메인 생성 및 이름 속성 추가

---
+++ src/main/java/shopping/domain/Product.java
@@ -0,0 +1,17 @@
+package shopping.domain;
+
+public class Product {
+    private String name;
+
+    public Product() {
+
+    }
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
