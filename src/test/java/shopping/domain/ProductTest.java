f78a34e9c8a406734a7289e75e46a1d3e79870ee
apdkeb <apdkeb@naver.com>
2026-03-06 16:40:20 +0900

    test(Product) : 상품가격 테스트 생성

--- src/test/java/shopping/domain/ProductTest.java
+++ src/test/java/shopping/domain/ProductTest.java
@@ -3,7 +3,10 @@ package shopping.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -37,10 +40,19 @@ class ProductTest {
 
     @ParameterizedTest
     @DisplayName("상품명이 비어있거나, 1자 미만이거나 15자를 초과하면 예외 발생")
-    @ValueSource(strings = {"", "  ", "상품입니다상품입니다상품입니다상"})
+    @NullAndEmptySource
+    @ValueSource(strings = {"  ", "상품입니다상품입니다상품입니다상"})
     void fail_invalidNameLength(String name) {
         assertThatThrownBy(() -> new Product(name))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    @DisplayName("상품 가격은 가진다")
+    void success_productPrice() {
+        BigDecimal price = new BigDecimal(15000);
+        Product product = new Product("피자", price);
+
+        assertThat(product.getPrice()).isEqualTo(price);
+    }
 }
