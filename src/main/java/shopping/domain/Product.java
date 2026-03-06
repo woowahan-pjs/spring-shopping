555b181b801ce4113e41b9705b36fd63fef640f8
apdkeb <apdkeb@naver.com>
2026-03-06 15:09:13 +0900

    feat(Product) : 상품명 길이 유효성 추가

--- src/main/java/shopping/domain/Product.java
+++ src/main/java/shopping/domain/Product.java
@@ -1,5 +1,7 @@
 package shopping.domain;
 
+import org.springframework.util.StringUtils;
+
 public class Product {
     private String name;
 
@@ -8,10 +10,21 @@ public class Product {
     }
 
     public Product(String name) {
+        validNameLength(name);
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    private void validNameLength(String name) {
+        if (!StringUtils.hasText(name)) {
+            throw new IllegalArgumentException("상품명은 필수입니다.");
+        }
+
+        if (name.length() > 15) {
+            throw new IllegalArgumentException("상품명은 1자 이상 15자 이하여야 합니다.");
+        }
     }
 }
