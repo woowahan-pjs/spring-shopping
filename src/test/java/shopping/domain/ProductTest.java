81928fc152b719ff8eef048ef320fd7bf3ff7186
apdkeb <apdkeb@naver.com>
2026-03-06 14:53:53 +0900

    test(Product) : 상품명 길이 테스트 추가

--- src/test/java/shopping/domain/ProductTest.java
+++ src/test/java/shopping/domain/ProductTest.java
@@ -2,6 +2,8 @@ package shopping.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -22,5 +24,14 @@ class ProductTest {
         Product product = new Product(name);
 
         assertThat(product.getName()).isEqualTo(name);
+    }
+
+    @ParameterizedTest
+    @DisplayName("상품명 길이는 최소 1자리 ~ 최대 15자리다")
+    @ValueSource(strings = {"상", "상품입니다", "상품입니다상품입니다상품입니다"})
+    void productNameMaxLength(String name) {
+        Product product = new Product(name);
+
+        assertThat(product.getName().length()).isEqualTo(name);
     }
 }
