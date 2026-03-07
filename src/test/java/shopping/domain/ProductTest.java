07c9f08419879166e3ae228bc72bc0729b0f6722
apdkeb <apdkeb@naver.com>
2026-03-07 15:52:59 +0900

    test(Product) : 테스트 픽스처로 하드코딩 개선

--- src/test/java/shopping/domain/ProductTest.java
+++ src/test/java/shopping/domain/ProductTest.java
@@ -12,21 +12,31 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class ProductTest {
+    private static final String VALID_NAME = "피자";
+    private static final BigDecimal VALID_PRICE = BigDecimal.ZERO;
+    private static final String VALID_IMAGE_URL = "http://a.com/a.jpg";
+
+    private Product createProduct(String name) {
+        return new Product(name, VALID_PRICE, VALID_IMAGE_URL);
+    }
+
+    private Product createProduct(String name, BigDecimal price) {
+        return new Product(name, price, VALID_IMAGE_URL);
+    }
 
     @Test
     @DisplayName("상품은 이름을 가진다")
     void productName() {
-        String name = "피자";
-        Product product = new Product(name, BigDecimal.ZERO, "http://a.com/a.jpg");
+        Product product = createProduct(VALID_NAME);
 
-        assertThat(product.getName()).isEqualTo(name);
+        assertThat(product.getName()).isEqualTo(VALID_NAME);
     }
 
     @ParameterizedTest
     @DisplayName("상품명 길이는 최소 1자리 ~ 최대 15자리다")
     @ValueSource(strings = {"상", "상품입니다", "상품입니다상품입니다상품입니다"})
     void productNameMaxLength(String name) {
-        Product product = new Product(name, BigDecimal.ZERO, "http://a.com/a.jpg");
+        Product product = createProduct(name);
 
         assertThat(product.getName()).isEqualTo(name);
     }
@@ -36,7 +46,7 @@ class ProductTest {
     @NullAndEmptySource
     @ValueSource(strings = {"  ", "상품입니다상품입니다상품입니다상"})
     void invalidNameLength(String name) {
-        assertThatThrownBy(() -> new Product(name, BigDecimal.ZERO, "http://a.com/a.jpg"))
+        assertThatThrownBy(() -> createProduct(name))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -44,14 +54,14 @@ class ProductTest {
     @ParameterizedTest
     @ValueSource(strings = {"(", ")", "[", "]", "+", "-", "&", "/", "_"})
     void productName_withSpecialChars(String specialChar) {
-        assertDoesNotThrow(() -> new Product(specialChar, new BigDecimal(0), "http://a.com/a.jpg"));
+        assertDoesNotThrow(() -> createProduct(specialChar));
     }
 
     @DisplayName("상품명에는 특수문자 (), [], +, -, &, /, _ 제외하면 사용할 수 없다.")
     @ParameterizedTest
     @ValueSource(strings = {"@", "!", "*", "#", "=", "{", "}", "\\", "|"})
     void productName_withDisallowedSpecialChars(String specialChar) {
-        assertThatThrownBy(() -> new Product(specialChar, BigDecimal.ZERO, "http://a.com/a.jpg"))
+        assertThatThrownBy(() -> createProduct(specialChar))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -59,7 +69,7 @@ class ProductTest {
     @DisplayName("상품은 가격을 가진다")
     void productPrice() {
         BigDecimal price = new BigDecimal(15000);
-        Product product = new Product("피자", price, "http://a.com/a.jpg");
+        Product product = createProduct(VALID_NAME, price);
 
         assertThat(product.getPrice()).isEqualTo(price);
     }
@@ -69,7 +79,7 @@ class ProductTest {
     void productPriceLessThanZero() {
         BigDecimal price = new BigDecimal(-1);
 
-        assertThatThrownBy(() -> new Product("피자", price, "http://a.com/a.jpg"))
+        assertThatThrownBy(() -> createProduct(VALID_NAME, price))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -79,7 +89,7 @@ class ProductTest {
     void productPriceExceedsMax() {
         BigDecimal price = new BigDecimal(1000000000);
 
-        assertThatThrownBy(() -> new Product("피자", price, "http://a.com/a.jpg"))
+        assertThatThrownBy(() -> createProduct(VALID_NAME, price))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -87,7 +97,7 @@ class ProductTest {
     @DisplayName("상품이미지는 URL 경로로 표현한다.")
     void productImage() {
         String url = "https://www.naver.com/test.jpg";
-        Product product = new Product("피자", new BigDecimal(15000), url);
+        Product product = new Product(VALID_NAME, VALID_PRICE, url);
 
         assertThat(product.getImageUrl()).isEqualTo(url);
     }
@@ -98,7 +108,7 @@ class ProductTest {
     @NullAndEmptySource
     @ValueSource(strings = {"hhpp://naver.com", "dkjksjkjfd"})
     void invalidImageUrl(String url) {
-        assertThatThrownBy(() -> new Product("피자", new BigDecimal(0), url))
+        assertThatThrownBy(() -> new Product(VALID_NAME, VALID_PRICE, url))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -106,14 +116,14 @@ class ProductTest {
     @DisplayName("상품이미지 파일 확장자는 jpg, jpeg, png, gif, svg만 가능")
     @ValueSource(strings = {"http://a.com/a.jpg", "http://a.com/a.jpeg", "http://a.com/a.png", "http://a.com/a.gif", "http://a.com/a.svg"})
     void productImageUrl_withValidExtension(String url) {
-        assertDoesNotThrow(() -> new Product("피자", new BigDecimal(0), url));
+        assertDoesNotThrow(() -> new Product(VALID_NAME, VALID_PRICE, url));
     }
 
     @ParameterizedTest
     @DisplayName("상품이미지 파일 확장자가 잘못되면 예외처리")
     @ValueSource(strings = {"http://a.com/a.txt", "http://a.com/a.mp3", "http://a.com/a", "http://a.com/a.pdf"})
     void productImageUrl_withInvalidExtension(String url) {
-        assertThatThrownBy(() -> new Product("피자", new BigDecimal(0), url))
+        assertThatThrownBy(() -> new Product(VALID_NAME, VALID_PRICE, url))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
