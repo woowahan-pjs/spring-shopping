da38948272edcc10a6b01ca9335006e06ee7bb07
cwangg897 <dhkdrb897@gmail.com>
2026-03-07 21:58:31 +0900

    feat(product): 상품 조회 기능 추가

---
+++ src/main/java/shopping/product/application/query/ProductQueryService.java
@@ -0,0 +1,33 @@
+package shopping.product.application.query;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shopping.common.exception.ApiException;
+import shopping.common.exception.ErrorType;
+import shopping.product.application.dto.ProductGetResponse;
+import shopping.product.domain.ProductErrorMessage;
+import shopping.product.domain.ProductRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ProductQueryService {
+
+    private final ProductRepository productRepository;
+
+    public ProductGetResponse getById(Long productId) {
+        return productRepository.findByIdNotDeleted(productId)
+            .map(ProductGetResponse::from)
+            .orElseThrow(() -> new ApiException(ProductErrorMessage.NOT_FOUND.getDescription(), ErrorType.NO_RESOURCE, HttpStatus.NOT_FOUND));
+    }
+
+    public List<ProductGetResponse> getAll() {
+        return productRepository.findAllNotDeleted().stream()
+            .map(ProductGetResponse::from)
+            .toList();
+    }
+
+}
