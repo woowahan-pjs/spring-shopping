9bd309e2b6e1a985a44da100eba0ca450379f5c0
jivebreaddev <jivebreaddev@gmail.com>
2026-03-07 19:54:59 +0900

    chore: initialize spring boot project

---
+++ src/main/kotlin/shopping/Application.kt
@@ -0,0 +1,22 @@
+package shopping
+
+import jakarta.annotation.PostConstruct
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
+import java.util.TimeZone
+
+@ConfigurationPropertiesScan
+@SpringBootApplication
+class Application {
+
+    @PostConstruct
+    fun started() {
+        // set timezone
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
