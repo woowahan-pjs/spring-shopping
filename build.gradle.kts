379c05c5a3af810c34f2e7a23da14a6de6287ea2
cwangg897 <dhkdrb897@gmail.com>
2026-03-07 19:05:15 +0900

    chore(gradle): jjwt, jbcrypt 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,10 @@ dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+    implementation("org.mindrot:jbcrypt:0.4")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
