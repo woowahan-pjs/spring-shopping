d275a47c5b22bb46ca685a0ba0a6972388b5a7c4
jivebreaddev <jivebreaddev@gmail.com>
2026-03-08 14:55:32 +0900

    feat: implement bad words validation in productService:CreateProduct

--- src/main/kotlin/shopping/application/product/ProductService.kt
+++ src/main/kotlin/shopping/application/product/ProductService.kt
@@ -3,13 +3,15 @@ package shopping.application.product
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import shopping.domain.product.Product
+import shopping.domain.product.ProductBadWordClient
 import shopping.domain.product.ProductRepository
 import shopping.support.error.CoreException
 import shopping.support.error.ErrorType
 import java.math.BigDecimal
 
 class ProductService(
-    private val productRepository: ProductRepository
+    private val productRepository: ProductRepository,
+    private val productBadWordClient: ProductBadWordClient
 ) {
     fun findByPaging(page: Int, size: Int): Page<Product> {
         return productRepository.findAll(PageRequest.of(page, size))
@@ -21,6 +23,7 @@ class ProductService(
     }
 
     fun create(name: String, price: BigDecimal, imageUrl: String): Product {
+        validateProfanity(name)
         val product = Product(
             name = name,
             price = price,
@@ -30,9 +33,16 @@ class ProductService(
     }
 
     fun update(id: Long, name: String, price: BigDecimal, imageUrl: String): Product {
+        validateProfanity(name)
         val product = findById(id)
         product.update(name, price, imageUrl)
         return productRepository.save(product)
+    }
+
+    private fun validateProfanity(name: String) {
+        if (productBadWordClient.containsProfanity(name)) {
+            throw CoreException(ErrorType.BAD_REQUEST, "상품 이름에 비속어를 포함할 수 없습니다.")
+        }
     }
 
     fun delete(id: Long) {
---
+++ src/main/kotlin/shopping/infrastructure/product/client/ProductBadWordClientAdapter.kt
@@ -0,0 +1,19 @@
+package shopping.infrastructure.product.client
+
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import shopping.domain.product.ProductBadWordClient
+
+@Component
+class ProductBadWordClientAdapter(
+    private val profanityWebClient: WebClient
+) : ProductBadWordClient {
+
+    override fun containsProfanity(text: String): Boolean {
+        return profanityWebClient.get()
+            .uri("/service/containsprofanity?text={text}", text)
+            .retrieve()
+            .bodyToMono(Boolean::class.java)
+            .block() ?: false
+    }
+}
