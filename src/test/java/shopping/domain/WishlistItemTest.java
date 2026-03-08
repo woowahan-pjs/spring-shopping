7cf693f6c09fba30ca25b7e95dd863ee27142215
apdkeb <apdkeb@naver.com>
2026-03-08 14:54:26 +0900

    test(WishlistItem) : 불필요한 코드 제거.

--- src/test/java/shopping/domain/WishlistItemTest.java
+++ src/test/java/shopping/domain/WishlistItemTest.java
@@ -4,45 +4,31 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-import static shopping.domain.ProductFixture.*;
 
 public class WishlistItemTest {
 
     @Test
     @DisplayName("위시리스트에 상품을 추가한다.")
     void addWishlistItem() {
-        Member member = createMember();
-        Product product = createProduct();
-        member.assignId(1L);
-        product.assignId(1L);
+        Long memberId = 1L;
+        Long productId = 1L;
+        WishlistItem wishlistItem = new WishlistItem(memberId, productId);
 
-        WishlistItem wishlistItem = new WishlistItem(member.getId(), product.getId());
-
-        assertThat(wishlistItem.getMemberId()).isEqualTo(member.getId());
-        assertThat(wishlistItem.getProductId()).isEqualTo(product.getId());
+        assertThat(wishlistItem.getMemberId()).isEqualTo(memberId);
+        assertThat(wishlistItem.getProductId()).isEqualTo(productId);
     }
 
     @Test
     @DisplayName("회원 아이디가 없으면 예외가 발생한다.")
     void invalidMemberId() {
-        Product product = createProduct();
-        product.assignId(1L);
-
-        assertThatThrownBy(() -> new WishlistItem(null, product.getId()))
+        assertThatThrownBy(() -> new WishlistItem(null, 1L))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("상품 아이디가 없으면 예외가 발생한다.")
     void invalidProductId() {
-        Member member = createMember();
-        member.assignId(1L);
-
-        assertThatThrownBy(() -> new WishlistItem(member.getId(), null))
+        assertThatThrownBy(() -> new WishlistItem(1L, null))
                 .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    private Member createMember() {
-        return new Member("test@gmail.com", "password");
     }
 }
