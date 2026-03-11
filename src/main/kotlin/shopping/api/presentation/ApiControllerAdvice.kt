b7a48799fd447185678b3b3fa8dd4017b613a465
devfancy <fancy.junyongmoon@gmail.com>
2026-03-11 18:58:31 +0900

    feat: 예외 처리에 대한 ApiControllerAdvice 클래스 추가

---
+++ src/main/kotlin/shopping/api/presentation/ApiControllerAdvice.kt
@@ -0,0 +1,25 @@
+package shopping.api.presentation
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.logging.LogLevel
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import shopping.support.error.CoreException
+import shopping.support.response.ApiResponse
+
+@RestControllerAdvice
+class ApiControllerAdvice {
+    private val log: Logger = LoggerFactory.getLogger(javaClass)
+
+    @ExceptionHandler(CoreException::class)
+    fun handleCoreException(e: CoreException): ResponseEntity<ApiResponse<Any>> {
+        when (e.errorType.logLevel) {
+            LogLevel.ERROR -> log.error("CoreException : {}", e.message, e)
+            LogLevel.WARN -> log.warn("CoreException : {}", e.message, e)
+            else -> log.info("CoreException : {}", e.message)
+        }
+        return ResponseEntity(ApiResponse.Companion.error(e.errorType, e.data), e.errorType.status)
+    }
+}
