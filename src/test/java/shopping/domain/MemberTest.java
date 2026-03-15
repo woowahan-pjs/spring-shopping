f83456ef6c8b1bc3ed77d4944ad6b2e87eb3afce
apocalypse7050@gmail.com <pota@codeindesign.kr>
2026-03-15 22:35:24 +0900

    test: Member 엔티티 생성시 필드 검증하는 테스트 코드

--- src/test/java/shopping/domain/MemberTest.java
+++ src/test/java/shopping/domain/MemberTest.java
@@ -1,5 +1,6 @@
 package shopping.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -7,6 +8,22 @@ import org.junit.jupiter.api.Test;
 import shopping.exception.NotValidException;
 
 class MemberTest {
+
+	@Test
+	void create_이메일_비밀번호_이름_필드가_정확히_설정된다() {
+		Member member = Member.create("test@test.com", "encryptedPassword", "테스터");
+
+		assertThat(member.getEmail()).isEqualTo("test@test.com");
+		assertThat(member.getPassword()).isEqualTo("encryptedPassword");
+		assertThat(member.getName()).isEqualTo("테스터");
+	}
+
+	@Test
+	void create_id는_null이다() {
+		Member member = Member.create("test@test.com", "encryptedPassword", "테스터");
+
+		assertThat(member.getId()).isNull();
+	}
 
 	@Test
 	void validPassword_비밀번호_일치_정상() {
