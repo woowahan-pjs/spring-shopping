7950840159394b943f46de161ad7f5126b20f2cd
apdkeb <apdkeb@naver.com>
2026-03-22 21:38:52 +0900

    refactor : @EntityListeners, @EnableJpaAuditing 추가
    - BaseEntity : @CreateDate, @LastModifiedDate 어노테이션 선언.

--- src/main/java/shopping/config/AppConfig.java
+++ src/main/java/shopping/config/AppConfig.java
@@ -1,11 +1,13 @@
 package shopping.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import shopping.auth.AdminInterceptor;
 import shopping.auth.AuthInterceptor;
 
+@EnableJpaAuditing
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
     private final AuthInterceptor authInterceptor;
