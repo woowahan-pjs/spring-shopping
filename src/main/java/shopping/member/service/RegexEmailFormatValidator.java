9427f4561c37f3634bc0a448d92c08d6e1bcab15
apdkeb <apdkeb@naver.com>
2026-03-22 21:33:53 +0900

    refactor : Pattern 상수로 캐싱
    - 매번 객체 생성에서 전역 변수로 재사용하게 변경.

--- src/main/java/shopping/member/service/RegexEmailFormatValidator.java
+++ src/main/java/shopping/member/service/RegexEmailFormatValidator.java
@@ -7,11 +7,11 @@ import java.util.regex.Pattern;
 @Component
 public class RegexEmailFormatValidator implements EmailFormatValidator {
     private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static final Pattern PATTERN = Pattern.compile(EMAIL_PATTERN);
 
     @Override
     public boolean isValid(String email) {
-        return Pattern.compile(EMAIL_PATTERN)
-                .matcher(email)
+        return PATTERN.matcher(email)
                 .matches();
     }
 }
