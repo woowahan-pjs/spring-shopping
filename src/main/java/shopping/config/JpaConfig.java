773bf8dff3a5770554acc46ae0d45d4b5e2e94a6
apdkeb <apdkeb@naver.com>
2026-03-22 22:30:08 +0900

    refactor : @EnableJpaAuditing -> JpaConfig로 분리
    - JpaConfig 생성
    - AppConfig : jpa 어노테이션 삭제

---
+++ src/main/java/shopping/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package shopping.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
